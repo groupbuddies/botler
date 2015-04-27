6e80423f54f376359bc368fcb37c7c321e7443cc
Benjamim Sonntag <benjamimsonntag@gmail.com>
2015-04-27 16:51:55 +0100

    Moves flash to a partial and creates a navbar.

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -4,12 +4,14 @@ RSpec.feature 'sign up' do
   scenario 'with valid data' do
     visit new_user_registration_path
 
-    fill_in 'Name', with: 'person'
-    fill_in 'Email', with: 'person@example.com'
-    fill_in 'Password', with: '12345678'
-    fill_in 'Password confirmation', with: '12345678'
+    within 'form' do
+      fill_in 'Name', with: 'person'
+      fill_in 'Email', with: 'person@example.com'
+      fill_in 'Password', with: '12345678'
+      fill_in 'Password confirmation', with: '12345678'
 
-    click_on 'Sign up'
+      click_on 'Sign up'
+    end
 
     expect(current_path).to eq root_path
     expect(page).to have_text 'Welcome! You have signed up successfully'
@@ -18,7 +20,9 @@ RSpec.feature 'sign up' do
   scenario 'with invalid data' do
     visit new_user_registration_path
 
-    click_on 'Sign up'
+    within 'form' do
+      click_on 'Sign up'
+    end
 
     expect(page).to have_text "Email can't be blank"
     expect(page).to have_text "Password can't be blank"
