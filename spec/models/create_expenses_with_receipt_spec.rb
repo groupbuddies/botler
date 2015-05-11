67b3ee08d01edb5b0bfa061a5a27507a0fea296c
Benjamim Sonntag <benjamimsonntag@gmail.com>
2015-05-11 13:25:59 +0100

    Adds more tests for CreateExpensesWithReceipt.

--- spec/models/create_expenses_with_receipt_spec.rb
+++ spec/models/create_expenses_with_receipt_spec.rb
@@ -10,13 +10,25 @@ describe CreateExpensesWithReceipt, type: :model do
     )
   end
 
-  let(:invalid_params) do
+  let(:params_without_picture) do
     user = create(:user)
     attributes_for(:expense).merge(user_id: user.id)
   end
 
+  let(:params_without_user) do
+    attributes_for(:expense).merge(
+      picture: Rack::Test::UploadedFile.new(File.join(
+        Rails.root, 'spec', 'support', 'images', 'receipt.jpg')))
+  end
+
   context '#create' do
     context 'with valid params' do
+      it 'returns true' do
+        creator = CreateExpensesWithReceipt.new(valid_params)
+
+        expect(creator.create).to be true
+      end
+
       it 'creates an expense' do
         creator = CreateExpensesWithReceipt.new(valid_params)
 
@@ -30,15 +42,41 @@ describe CreateExpensesWithReceipt, type: :model do
       end
     end
 
-    context 'with invalid params' do
+    context 'without a picture' do
+      it 'returns false' do
+        creator = CreateExpensesWithReceipt.new(params_without_picture)
+
+        expect(creator.create).to be false
+      end
+
       it 'does not create expenses' do
-        creator = CreateExpensesWithReceipt.new(invalid_params)
+        creator = CreateExpensesWithReceipt.new(params_without_picture)
 
         expect { creator.create }.not_to change { Expense.count }
       end
 
       it 'does not create receipts' do
-        creator = CreateExpensesWithReceipt.new(invalid_params)
+        creator = CreateExpensesWithReceipt.new(params_without_picture)
+
+        expect { creator.create }.not_to change { Receipt.count }
+      end
+    end
+
+    context 'without a user' do
+      it 'returns false' do
+        creator = CreateExpensesWithReceipt.new(params_without_user)
+
+        expect(creator.create).to be false
+      end
+
+      it 'does not create expenses' do
+        creator = CreateExpensesWithReceipt.new(params_without_user)
+
+        expect { creator.create }.not_to change { Expense.count }
+      end
+
+      it 'does not create receipts' do
+        creator = CreateExpensesWithReceipt.new(params_without_user)
 
         expect { creator.create }.not_to change { Receipt.count }
       end
