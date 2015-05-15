13c4a117e35c9f6c36de90608ae9564ddac26a84
Benjamim Sonntag <benjamimsonntag@gmail.com>
2015-05-15 17:33:33 +0100

    Adds HQ sign in.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,14 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable,
+    :registerable,
+    :recoverable,
+    :rememberable,
+    :trackable,
+    :validatable,
+    :omniauthable,
+    omniauth_providers: [:headquarters]
 
   has_many :expenses
   has_many :periodic_expenses
