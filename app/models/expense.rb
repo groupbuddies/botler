a259d0fc5eb5f0d0d749873af04365bb63cee626
Benjamim Sonntag <benjamimsonntag@gmail.com>
2015-05-25 17:35:24 +0100

    Updates expense table style.

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -7,4 +7,22 @@ class Expense < ActiveRecord::Base
   validates :description, :user, :amount, :paid_on, :category, presence: true
   validates :paid_on, date: { before_or_equal_to: proc { Date.today } }
   validates_associated :receipt
+
+  def format_amount
+    '€' + amount.to_s
+  end
+
+  def format_vat
+    vat.to_s + '%' unless vat.nil?
+  end
+
+  def format_amount_w_vat
+    '€' + amount_w_vat.to_s unless vat.nil?
+  end
+
+  private
+
+  def amount_w_vat
+    amount + (amount * (vat / 100))
+  end
 end
