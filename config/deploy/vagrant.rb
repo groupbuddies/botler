1cf7bec1d472ecb12acf32bd99bc793c7d990a28
Benjamim Sonntag <benjamimsonntag@gmail.com>
2015-06-05 14:02:24 +0100

    Updates vagrant config.

--- config/deploy/vagrant.rb
+++ config/deploy/vagrant.rb
@@ -1,2 +1,9 @@
-server '10.6.6.6', user: 'vagrant', roles: %w{web app db}, primary: true
-set :ssh_options, { forward_agent: true, keys: ['~/.vagrant.d/insecure_private_key'] }
+server '10.6.6.6',
+  user: 'vagrant',
+  roles: %w{web app db},
+  primary: true,
+  ssh_options: {
+    keys: %w(~/.vagrant.d/insecure_private_key),
+    forward_agent: true,
+  }
+
